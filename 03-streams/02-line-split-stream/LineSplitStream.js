e0dd91331073771236ad5e399e129cda4ba7c89d
pshchelukhin <pshchelukhin@phoenixit.ru>
2024-01-09 10:43:20 +0300

    add solution for 03-streams 02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,29 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
-  }
-
-  _transform(chunk, encoding, callback) {
+    this.resStr = '';
   }
 
   _flush(callback) {
+    callback(null, this.resStr);
+  }
+
+  _transform(chunk, encoding, callback) {
+    const curChankStr = chunk.toString('utf-8')
+    this.resStr += chunk.toString('utf-8')
+    if (~curChankStr.indexOf(os.EOL)) {
+      const arrRows = this.resStr.split(os.EOL)
+      if (arrRows.length > 2) {
+        arrRows.forEach(row => {
+          this.push(row);
+        })
+        this._flush = undefined;
+      } else {
+        this.resStr = arrRows[1] || '';
+        this.push(arrRows[0]);
+      }
+    }
+    callback();
   }
 }
 
